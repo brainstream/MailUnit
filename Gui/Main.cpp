a39d8f1b8c2a83ddc4855eca43761ec59169976e
brainstream <brainstream@yandex.ru>
2014-11-09 22:59:21 +0300

    Projects structure has been refactored. The Gui project has been added.

---
+++ Gui/Main.cpp
@@ -0,0 +1,28 @@
+/***********************************************************************************************
+ *                                                                                             *
+ * This file is part of MailUnit.                                                              *
+ *                                                                                             *
+ * MailUnit is free software: you can redistribute it and/or modify it under the terms of      *
+ * the GNU General Public License as published by the Free Software Foundation,                *
+ * either version 3 of the License, or (at your option) any later version.                     *
+ *                                                                                             *
+ * MailUnit is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY;      *
+ * without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  *
+ * See the GNU General Public License for more details.                                        *
+ *                                                                                             *
+ * You should have received a copy of the GNU General Public License along with MailUnit.      *
+ * If not, see <http://www.gnu.org/licenses/>.                                                 *
+ *                                                                                             *
+ ***********************************************************************************************/
+
+#include <QtGui/QApplication>
+#include <QtGui/QMainWindow>
+
+int main(int _argc, char ** _argv)
+{
+    QApplication app(_argc, _argv);
+    QMainWindow wnd;
+    wnd.show();
+    app.exec();
+    return 0;
+}
